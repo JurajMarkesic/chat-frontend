96a6915a9c7a4ec2dd2a393a60c78c64dd9eeb9b
JurajMarkesic <jmarkesic@tvz.hr>
2022-03-04 10:32:03 +0100

    Added google 0auth.

--- src/main.ts
+++ src/main.ts
@@ -6,17 +6,28 @@ import "virtual:windi.css";
 
 import App from "./App.vue";
 import router from "./router";
+import GAuth from "vue3-google-oauth2";
 
 const app = createApp(App);
 app.provide(
   "socket",
-  io("192.168.49.2:31621", {
+  io("http://192.168.49.2:32489", {
     transports: ["websocket"],
     upgrade: false,
-  }),
+  })
 );
 
 app.use(createPinia());
 app.use(router);
+
+const gAuthOptions = {
+  clientId:
+    "719626904664-r57cogjcnqee8duc49o1epjmkbb6o7dh.apps.googleusercontent.com",
+  scope: "email",
+  prompt: "consent",
+  fetch_basic_profile: true,
+};
+app.use(GAuth, gAuthOptions);
+app.provide("GAuth", app.config.globalProperties.$gAuth);
 
 app.mount("#app");
